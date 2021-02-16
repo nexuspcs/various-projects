c2183b6d7937a8e2aaf7f42c4d0346c86760a937
Nexus PCs <nexus.custom.pcs.aus@gmail.com>
2021-02-16 13:34:56 +1100

    Initial Commit

---
+++ Fake_Virus_SHELL.sh
@@ -0,0 +1,33 @@
+#  Fake_Virus_SHELL.sh
+#
+#
+#  Created by Nexus PCs on 16/2/21.
+#
+
+  echo "Loading virus.dmg - 10%"
+  sleep 0.3
+  echo "Loading virus.dmg - 20%"
+  sleep 2
+  echo "Loading virus.dmg - 22%"
+  sleep 5
+  echo "Loading virus.dmg - 40%"
+  sleep 6
+  echo "Loading virus.dmg - 42%"
+  sleep 1
+  echo "Loading virus.dmg - 50%"
+  sleep 5
+  echo "Loading virus.dmg - 69%"
+  sleep 0.69
+  echo "Loading virus.dmg - 79%"
+  sleep 1
+  echo "Loading virus.dmg - 80%"
+  sleep 1
+  echo "Loading virus.dmg - 95%"
+  sleep 8
+  echo "Loading virus.dmg - 99.5%"
+  sleep 8
+  echo "Loading virus.dmg - 100%"
+  sleep 8
+  echo "Loading virus.dmg - COMPLETE, restart now!"
+  sleep 8
+done
